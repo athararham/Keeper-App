cd5420138ec42f60f9d19580dd2bb1c5f2207b6c
athararham <68887118+athararham@users.noreply.github.com>
2020-07-28 14:07:15 +0530

    Keeper App part 1 complete

---
+++ components/footer.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+var date = new Date();
+var year = date.getFullYear();
+
+function Footer() {
+  return (
+    <footer>
+      <p>copyright &copy; {year} </p>
+    </footer>
+  );
+}
+export default Footer;
